f23c10117e6e894f4619cfde2570aad6f2aeacf8
ndovooformation <formation@ndovoo.com>
2026-01-30 09:02:39 +0300

    Installer le package EJS; Créer les vues EJS; Retourner les VUES EJS dans les API REST

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// Je précise que les vues sont dans le dossier views
+app.set('views', './views');
+
+// Je précise que nous utilisons le moteur EJS pour les vues
+app.set('view engine', 'ejs');
 
 // API ROUTE pour la page racine : localhost:3004/
 app.get('/', (req, res) => {
@@ -14,14 +19,20 @@ app.get('/', (req, res) => {
 app.get('/api/accueil', (req, res) => {
     console.log("Je passe dans /api/accueil");
 
+    res.render('accueil');
     // Le type d'encodage du texte retourné en réponse
-    res.writeHead(200, { "content-type": "text/html;charset=utf-8"}); 
+    //res.writeHead(200, { "content-type": "text/html;charset=utf-8"}); 
 
     // Le contenu qui sera affiché côté navigateur web
-    res.write("<p> Je suis à l'accueil</p>");
+    //res.write("<p> Je suis à l'accueil</p>");
 
     // Fin de la réponse
-    res.end();
+    //res.end();
+});
+
+app.get('/api/equipe', (req, res) => {
+    console.log("Je passe dans la route API REST /api/equipe");
+    res.render('equipe');
 });
 
 
